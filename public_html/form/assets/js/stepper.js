ad481594fc4d1bbe4b28d3cc8a638e8f3f9ec05d
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-02-29 13:27:34 +0800

    stepper css and js button for bedroom and bathroom in bool-now.php added

--- public_html/form/assets/js/stepper.js
+++ public_html/form/assets/js/stepper.js
@@ -1,47 +1,165 @@
 
-var bed = 0;
-var bath = 0;
-var bedRoomPlural = 's';
-var string = '';
 
-/** */
-$(document).ready(function(){
-    $("#bed").val('1 bedroom');
-});
-
-/** */
-var i = $("#bed");
-
-/** */
-i.TouchSpin({});
-i.on("touchspin.on.startupspin", function () {
+bed('#bed');
+bath('#bath');
 
 
-    if(bed < 6) {
-        bed++;
-        str = bed + ' bedroom';
-    } else {
-        str = bed + ' bedroom';
-    }
 
-    if(bed > 1) {
-        str += bedRoomPlural;
-    }
+function bed(id) {
+    var bed = 1;
+    var bath = 1;
+    var bedRoomPlural = 's';
+    var string = '';
+    var maxValue = 6;
+    var minValue = 1;
+    var itemName = 'bedroom';
+    var initValue = '1 bedroom';
+    //var fieldCurrentValue = $(id).val();
+    //id = '#bed';
 
-    $('#bed').val(str);
-});
-i.on("touchspin.on.startdownspin", function () {
-    if(bed > 1 ) {
-        bed--;
-        str = bed + ' bedroom';
-    } else {
+    /** */
+    $(document).ready(function(){
+        $(id).val(initValue);
+    });
 
-        str = bed + ' bedroom';
-    }
+    /** */
+    var i = $(id);
 
-    if(bed > 1) {
-        str += bedRoomPlural;
-    }
+    /** */
+    i.TouchSpin({});
+    i.on("touchspin.on.startupspin", function () {
 
-    $('#bed').val(str);
-});
+
+        if(bed < maxValue) {
+            bed++;
+            str = bed + ' ' + itemName;
+        } else {
+            str = bed + ' ' + itemName;
+        }
+
+        if(bed > 1) {
+            str += bedRoomPlural;
+        }
+
+        $(id).val(str);
+        //$('#bed-room-stepper').val(str);
+    });
+    i.on("touchspin.on.startdownspin", function () {
+        if(bed > minValue ) {
+            bed--;
+            str = bed + ' ' + itemName;
+        } else {
+
+            str = bed + ' ' + itemName;
+        }
+
+        if(bed > 1) {
+            str += bedRoomPlural;
+        }
+        $(id).val(str);
+        //$('#bed-room-stepper').val(str);
+    });
+
+    //i.on("touchspin.on.stopspin", function () {
+    //
+    //    console.log("bed " + fieldCurrentValue + "max value " + maxValue);
+    //    if(fieldCurrentValue > maxValue) {
+    //        alert("max value");
+    //        $(id).val(initValue);
+    //    }
+    //});
+}
+function bath(id) {
+    var bed = 1;
+    var bath = 1;
+    var bedRoomPlural = 's';
+    var string = '';
+    var maxValue = 11;
+    var minValue = 1;
+    var itemName = 'bathroom';
+    var initValue = '1 bathroom';
+    //id = '#bed';
+
+    /** */
+    $(document).ready(function(){
+        $(id).val(initValue);
+    });
+
+    /** */
+    var i = $(id);
+
+    /** */
+    i.TouchSpin({});
+    i.on("touchspin.on.startupspin", function () {
+
+
+        if(bed < maxValue) {
+            bed++;
+            str =  bathRoomUnit(bed) + ' ' + itemName;
+        } else {
+            str =  bathRoomUnit(bed) + ' ' + itemName;
+        }
+
+        if(bed > 1) {
+            str += bedRoomPlural;
+        }
+
+        $(id).val(str);
+        //$('#bed-room-stepper').val(str);
+    });
+    i.on("touchspin.on.startdownspin", function () {
+        if(bed > minValue ) {
+            bed--;
+            str = bathRoomUnit(bed) + ' ' + itemName;
+        } else {
+            str = bathRoomUnit(bed) + ' ' + itemName;
+        }
+
+        if(bed > 1) {
+            str += bedRoomPlural;
+        }
+
+
+        $(id).val(str);
+        //$('#bed-room-stepper').val(str);
+    });
+    //
+    //i.on("touchspin.on.stopspin", function () {
+    //    if(bed > maxValue) {
+    //
+    //        alert("max value");
+    //        $(id).val(initValue);
+    //
+    //
+    //    }
+    //});
+
+    calculateFirstCleanByKeepItCleanGetCleanMoveInOut();
+
+}
+
+
+function bathRoomUnit(index) {
+
+    var unit = new Array("", "1", "1.5", "2", "2.5", "3", "3.5", "4", "4.5", "5.5", "1.5", "6");
+
+    return unit[index];
+}
+//
+//<option value="1">1 bathroom</option>
+//<option value="1.5">1.5 bathroom</option>
+//
+//<option value="2" selected>2 bathrooms</option>
+//<option value="2.5">2.5 bathroom</option>
+//
+//<option value="3">3 bathrooms</option>
+//<option value="3.5">3.5 bathroom</option>
+//
+//<option value="4">4 bathrooms</option>
+//<option value="4.5">4.5 bathroom</option>
+//
+//<option value="5.5">5.5 bathrooms</option>
+//<option value="1.5">1.5 bathroom</option>
+//
+//<option value="6">6 bathrooms</option>
+//}
