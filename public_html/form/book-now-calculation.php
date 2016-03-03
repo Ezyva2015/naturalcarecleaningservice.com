e2013816e462af7b2d3503fdd1795ee2f611e3f1
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-03-04 00:01:30 +0800

    add new calculation function to book-now-calculation.php

--- public_html/form/book-now-calculation.php
+++ public_html/form/book-now-calculation.php
@@ -677,11 +677,11 @@ $(function() {
 		return parseInt(totalBasePrice);
 	}
 
-	function calculateWeekly() { return basePrice() * weeklyPrice(); }
+//	function calculateWeekly() { return basePrice() * weeklyPrice(); }
 
-	function calculateEvery2weeks() { return basePrice() * every2WeeksPrice(); }
+//	function calculateEvery2weeks() { return basePrice() * every2WeeksPrice(); }
 
-	function calculateEvery4weeks() { return basePrice() * every4WeeksPrice(); }
+//	function calculateEvery4weeks() { return basePrice() * every4WeeksPrice(); }
 
 	function weeklyPrice(){ return parseFloat("0.65"); }
 
@@ -759,6 +759,107 @@ $(function() {
  		$(firstCleanId).text(firstclean);
 
 	}
+
+	/**
+	 * New coding
+	 */
+	initialize();
+				function initialize() {
+
+		console.log("initialized : beds " +  memberInputBeds() + " bath " + memberInputBath() + " sqft " + memberInputSqft() );
+		console.log("square foot calculation " + Math.round(calculateSqftCalc()));
+		console.log("calculate Base Price " +  calculateBasePrice());
+		console.log("calculate weekly " +  calculateWeekly());
+		console.log("calculate 2 weeks " + calculateEvery2Weeks());
+		console.log("calculate 4 weeks " + calculateEvery4Weeks());
+
+		console.log("Get it clean " + calculateGetItClean());
+		console.log("Deep clean " + calculateDeepClean());
+		console.log("Keep Clean " +  calculateMoveInOut());
+
+	}
+
+	// User input
+		function memberInputBeds() { return convertNumber('1'); }
+
+		function memberInputBath() { return convertNumber('1'); }
+
+		function memberInputSqft() { return convertNumber('2000'); }
+
+	// Admin Input
+		function adminInputBeds() { return convertNumber('6.1'); }
+
+		function adminInputBath() { return convertNumber('14'); }
+
+		function adminInputSqftBase() { return convertNumber('1000'); }
+
+		function adminInputBaseValue() { return convertNumber('99'); }
+
+		function adminInputWeekly() { return convertNumber('0.65'); }
+
+		function adminInputEvery2Weeks() { return convertNumber('0.7'); }
+
+		function adminInputEvery4Weeks() { return convertNumber('0.75'); }
+
+		function adminInputGetItClean() { return convertNumber('1.4'); }
+
+		function adminInputDeepClean() { return convertNumber('1.5'); }
+
+		function adminInputMoveInOut() { return convertNumber('1.75'); }
+
+	// Calculation
+		function calculateSqftCalc() {
+			var multiplier1 = 100;
+			var multiplier2 = 3;
+			var answer = 0;
+			if(memberInputSqft() > adminInputSqftBase()) {
+				answer = (memberInputSqft() - adminInputSqftBase())/multiplier1*multiplier2;
+			}
+			return answer;
+		}
+
+		function calculateBasePrice() {
+
+			var totalBedsPrice     = adminInputBeds();
+			var totalBathRoOmPrice = adminInputBath();
+			var totalBaseValue     = adminInputBaseValue();
+			var totalSquareFootCalculation = calculateSqftCalc();
+			var totalBathRoom = memberInputBath();
+			var totalBeds = memberInputBeds();
+			//totalSquareFootCalculation = M3;
+			var totalBasePrice = (totalBeds*totalBedsPrice) + (totalBathRoom*totalBathRoOmPrice) + totalBaseValue+totalSquareFootCalculation;
+			return convertNumber(totalBasePrice);
+		}
+
+		function calculateWeekly() {
+			return convertNumber(calculateBasePrice() * adminInputWeekly());
+		}
+
+		function calculateEvery2Weeks() {
+			return  convertNumber(calculateBasePrice() * adminInputEvery2Weeks() );
+		}
+
+		function calculateEvery4Weeks() {
+			return convertNumber(calculateBasePrice() * adminInputEvery4Weeks() );
+		}
+
+		function calculateGetItClean() {
+			return convertNumber(calculateBasePrice() *  adminInputGetItClean());
+
+		}
+
+		function calculateDeepClean() {
+			return convertNumber(calculateBasePrice() *  adminInputDeepClean());
+		}
+
+		function calculateMoveInOut() {
+			return convertNumber(calculateBasePrice() *  adminInputMoveInOut());
+		}
+
+	// Helper
+	 	function convertNumber(number) { return parseFloat(number) }
+
+
 
 
 
@@ -1573,6 +1674,8 @@ $(function() {
 
 	<!-- BEGIN BODY -->
 	<body style="background-color: white">
+
+		<input type="button" onclick="initialize()" value="calculate" />
 				<H1> Booking calculation..</H1>
 
 	    <div style="background: #ddd; height: 7px;"></div>				<!-- TOP ROW HEADER -->
